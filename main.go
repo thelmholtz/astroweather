394d9e32b4cbe858250ffdfbfb7f735d10e0563a
Tomás Guinzburg <tomasguinzburg@gmail.com>
2018-12-26 19:05:45 -0300

    Refactored the router and db to use appengine pkgs Added app.yaml Changed backend and API to english; forced days to be int Days now get normalized first to reduce the dataset and allow querying values out of the range. There is still a fallback

--- main.go
+++ main.go
@@ -1,7 +1,131 @@
 package main
 
+import (
+	"encoding/json"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+
+	"net/http"
+	"strconv"
+
+	"github.com/thelmholtz/astroweather/model"
+	"github.com/thelmholtz/except"
+)
+
 func main() {
-	var a = App{}
-	a.Initialize()
-	a.Run(":8080")
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/predict", GenerateForecast)
+	http.HandleFunc("/clima", GetForecast)
+	appengine.Main()
+}
+
+//Index is a quick description of the service
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the home screen. Try issuing a GET to /predict to generate predictions, or /clima?dia={int} to get the prediction for a given day"))
+}
+
+//normalizeDays takes a day from minint to maxint and returns it's absolute modulo 360 (0 to 359); which is the amount of days in a Ferengian year.
+//As the weather is yearly-periodical for the whole planetary system, normalization allows us to store a small sample of data and yet predict the weather for a theoretically infinite range.
+//For instance, negative values can be queried with consistent results.
+func normalizeDay(i int) int {
+	for i < 0 {
+		return (360 + (i % 360)) % 360 //Go's implementation of modulo yields negative numbers if the operand is negative; we need to normalize this again after shifting them from the range (-359, 0) to (0, 359)
+	}
+	return i % 360
+}
+
+//GetForecast is the handler function for `clima` endpoint.
+func GetForecast(w http.ResponseWriter, r *http.Request) {
+
+	enc := json.NewEncoder(w)
+	ctx := appengine.NewContext(r)
+
+	day, err := strconv.Atoi(r.FormValue("dia"))
+	if err != nil {
+		log.Errorf(ctx, "Day was not castable to int: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		enc.Encode(except.New("TYPE", "Day must be a number"))
+	}
+
+	q := datastore.NewQuery("Forecast").Filter("Day =", normalizeDay(day)).Order("Day").Limit(1)
+
+	var forecasts []model.Forecast
+
+	log.Infof(ctx, "Getting Forecast")
+
+	if _, err := q.GetAll(ctx, &forecasts); err != nil {
+
+		//If the query fails, either becacuse the value was not found, or because it's out of the simulated range of 10 years, we fallback to making the prediction numerically.
+		//If the reason the query failed is the record was not found, we could just insert the record after replying to keep it cached and avoid future computation.
+		f := new(model.Forecast)
+		f.Day = day
+
+		log.Errorf(ctx, "Query failed for day: %v, Error: %v", f.Day, err)
+
+		if err := forecasts[0].Predict(); err != nil {
+			log.Errorf(ctx, "Error making fallback predictions: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			enc.Encode(err)
+			return
+		}
+	}
+	log.Infof(ctx, "Forecasts: %v", forecasts)
+
+	forecasts[0].Day = day
+
+	enc.Encode(forecasts[0])
+}
+
+//GenerateForecast returns a list of all the days and their prediction
+func GenerateForecast(w http.ResponseWriter, r *http.Request) {
+
+	ctx := appengine.NewContext(r)
+	enc := json.NewEncoder(w)
+
+	forecasts := make([]model.Forecast, 360) //Simula el clima para los proximos 10 años Ferengienses (duran 360 dias)
+
+	log.Infof(ctx, "Generating predictions")
+	for i := range forecasts {
+		f := model.Forecast{Day: i, Weather: "normal"}
+		if err := f.Predict(); err != nil {
+			log.Errorf(ctx, "Error generating predictions: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			enc.Encode(err)
+			return
+		}
+
+		forecasts[i] = f
+	}
+
+	var currentEntries []model.Forecast
+
+	log.Infof(ctx, "Querying current entries")
+	keys, err := datastore.NewQuery("Forecast").GetAll(ctx, &currentEntries)
+	if err != nil {
+		log.Errorf(ctx, "Error querying the current dataset: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	log.Infof(ctx, "Deleting the current entries")
+	for _, k := range keys {
+		if err := datastore.Delete(ctx, k); err != nil {
+			log.Errorf(ctx, "Error deleting the entry: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+
+	log.Infof(ctx, "Inserting records")
+	for _, f := range forecasts {
+		k := datastore.NewIncompleteKey(ctx, "Forecast", nil)
+		if k, err = datastore.Put(ctx, k, &f); err != nil {
+			log.Errorf(ctx, "Inserting forecast: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	log.Infof(ctx, "%v records inserted correctly: %v", len(forecasts), forecasts)
+	enc.Encode(forecasts)
 }
--- model/forecast.go
+++ model/forecast.go
@@ -1,44 +1,34 @@
 package model
 
 import (
-	"log"
-	"strconv"
-
 	"github.com/thelmholtz/astroweather/planets"
 	"github.com/thelmholtz/except"
 )
 
 //Forecast represents a day-weather value pair and it's json mapping.
 type Forecast struct {
-	D       string `bson:"day" json:"dia"`
-	Weather string `bson:"weather" json:"clima"`
+	Day     int    `bson:"day" json:"day"`
+	Weather string `bson:"weather" json:"weather"`
 }
 
 //Predict sets the Weather for this Day with an accurate prediction. Overwrites any previous value.
-func (forecast *Forecast) Predict() except.E {
-
-	key, err := strconv.Atoi(forecast.D)
-
-	if err != nil {
-		log.Print(err)
-		return except.New("TYPE", "El dia tiene que ser numerico")
-	}
+func (f *Forecast) Predict() except.E {
 
 	switch {
-	case key < 0:
+	case f.Day < 0:
 		err := except.New("VALUE", "El dia tiene que ser positivo")
 		return err
-	case isDry(key):
-		forecast.Weather = "sequia"
+	case isDry(f.Day):
+		f.Weather = "drought"
 		return nil
-	case isOptimal(key):
-		forecast.Weather = "optimo"
+	case isOptimal(f.Day):
+		f.Weather = "optimal"
 		return nil
-	case isRaining(key):
-		forecast.Weather = "lluvia"
+	case isRaining(f.Day):
+		f.Weather = "rain"
 		return nil
 	default:
-		forecast.Weather = "normal"
+		f.Weather = "normal"
 		return nil
 	}
 }
